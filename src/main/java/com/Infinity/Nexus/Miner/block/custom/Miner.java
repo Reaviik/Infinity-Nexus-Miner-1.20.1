23dc6588441a478b51c77aafeb795b9694ff1380
Reaviik <reavik_gamer@hotmail.com>
2025-05-27 17:18:57 -0300

    1.20.1-forge

---
+++ src/main/java/com/Infinity/Nexus/Miner/block/custom/Miner.java
@@ -0,0 +1,145 @@
+package com.Infinity.Nexus.Miner.block.custom;
+
+import com.Infinity.Nexus.Miner.block.custom.common.CommonUpgrades;
+import com.Infinity.Nexus.Miner.block.entity.MinerBlockEntity;
+import com.Infinity.Nexus.Miner.block.entity.ModBlockEntities;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Miner extends BaseEntityBlock {
+
+    public static IntegerProperty LIT = IntegerProperty.create("lit", 0, 17);
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public Miner(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return makeShape();
+    }
+    public VoxelShape makeShape(){
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0, 0.1875, 0, 1, 0.25, 1), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0, 0.3125, 0, 1, 1, 1), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.0625, 0.125, 0.0625, 0.938125, 0.3125, 0.938125), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0, 0, 0, 1, 0.125, 1), BooleanOp.OR);
+
+        return shape;
+    }
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+    @Override
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING, LIT);
+    }
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof MinerBlockEntity) {
+                ((MinerBlockEntity) blockEntity).drops();
+            }
+        }
+
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState pState, Level pLevel, @NotNull BlockPos pPos, @NotNull Player pPlayer, @NotNull InteractionHand pHand, @NotNull BlockHitResult pHit) {
+        CommonUpgrades.setUpgrades(pLevel, pPos, pPlayer);
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return new MinerBlockEntity(pPos, pState);
+    }
+
+
+    public void spawnParticles(BlockState pState, Level pLevel, BlockPos pPos, ParticleOptions source) {
+        pLevel.addParticle(source, pPos.getX() + 0.5D, pPos.getY() +0.5D, pPos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if(pLevel.isClientSide()) {
+            return null;
+        }
+
+        return createTickerHelper(pBlockEntityType, ModBlockEntities.MINER_BE.get(),
+                (pLevel1, pPos, pState1, pBlockEntity) -> pBlockEntity.tick(pLevel1, pPos, pState1));
+    }
+
+    @Override
+    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, @Nullable LivingEntity pPlacer, ItemStack pStack) {
+        if (pPlacer instanceof Player) {
+            Player player = (Player) pPlacer;
+            MinerBlockEntity minerBlockEntity = (MinerBlockEntity) pLevel.getBlockEntity(pPos);
+            minerBlockEntity.setOwner(player);
+        }
+        super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> components, TooltipFlag flag) {
+            if (Screen.hasShiftDown()) {
+                components.add(Component.translatable("item.infinity_nexus_miner.miner_description"));
+            } else {
+                components.add(Component.translatable("tooltip.infinity_nexus.pressShift"));
+            }
+
+            super.appendHoverText(stack, level, components, flag);
+
+    }
+}
---
+++ src/main/java/com/Infinity/Nexus/Miner/utils/ModUtilsMiner.java
@@ -0,0 +1,67 @@
+package com.Infinity.Nexus.Miner.utils;
+
+import com.Infinity.Nexus.Core.fakePlayer.IFFakePlayer;
+import com.Infinity.Nexus.Miner.config.Config;
+import com.Infinity.Nexus.Miner.item.ModItemsMiner;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ModUtilsMiner {
+
+    private static IFFakePlayer player;
+    public static ItemStack getCrystalType(int value) {
+        return switch (value) {
+            case 2 -> new ItemStack(ModItemsMiner.MARINE_CRYSTAL.get());
+            case 3 -> new ItemStack(ModItemsMiner.CITRIUM_CRYSTAL.get());
+            case 4 -> new ItemStack(ModItemsMiner.RUBIUM_CRYSTAL.get());
+            case 5 -> new ItemStack(ModItemsMiner.DEMETRIUM_CRYSTAL.get());
+            case 6 -> new ItemStack(ModItemsMiner.AGATE_CRYSTAL.get());
+            case 7 -> new ItemStack(ModItemsMiner.DARIUM_CRYSTAL.get());
+            case 8 -> new ItemStack(ModItemsMiner.TERMURIUM_CRYSTAL.get());
+            default -> new ItemStack(ModItemsMiner.AMBER_CRYSTAL.get());
+        };
+    }
+    public static Direction getLinkedSide(String side) {
+        return switch (side) {
+            case "up" -> Direction.UP;
+            case "down" -> Direction.DOWN;
+            case "north" -> Direction.NORTH;
+            case "south" -> Direction.SOUTH;
+            case "west" -> Direction.WEST;
+            case "east" -> Direction.EAST;
+            default -> Direction.UP;
+        };
+    }
+
+    public static ItemStack getDrop(ItemStack stack, Level level, BlockPos pos, ItemStack pickaxe) {
+        if (!(stack.getItem() instanceof BlockItem)) {
+            return null;
+        }
+        if (player == null) {
+            player = new IFFakePlayer((ServerLevel) level);
+        }
+        Block block = ((BlockItem) stack.getItem()).getBlock();
+        AtomicReference<ItemStack> dropItem = new AtomicReference<ItemStack>(ItemStack.EMPTY);
+
+        List<ItemStack> drops = new ArrayList<>(Block.getDrops(block.defaultBlockState(), (ServerLevel) level, pos, null, player, pickaxe));
+
+        drops.forEach(stack1 -> {
+            int drop = new Random().nextInt(drops.size());
+            dropItem.set(drops.get(drop).copy());
+
+        });
+        return dropItem.get();
+    }
+}
